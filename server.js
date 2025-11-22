c3708a417600d17c943889d46bc2985796fb214d
wishromo <hinasaeed1212@gmail.com>
2025-11-22 22:53:05 +0500

    creating server

---
+++ server.js
@@ -0,0 +1,44 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  const filePath = path.join(
+    __dirname,
+    req.url === "/" ? "index.html" : req.url
+  );
+  console.log(filePath);
+  const extName = String(path.extname(filePath)).toLowerCase();
+  const mimeTypes = {
+    ".html": "text/html",
+    ".js": "text/javascript",
+    ".css": "text/css",
+    ".json": "application/json",
+    ".png": "image/png",
+    ".jpg": "image/jpg",
+    ".gif": "image/gif",
+    ".wav": "audio/wav",
+  };
+
+  const contentType = mimeTypes[extName] || "application/octet-stream";
+
+  fs.readFile(filePath, (error, content) => {
+    if (error) {
+      if (error.code == "ENOENT") {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        res.end("404 file not found Brooooo!");
+      } else {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end(`Server Error: ${error.code}`);
+      }
+    } else {
+      res.writeHead(200, { "Content-Type": contentType });
+      res.end(content, "utf-8");
+    }
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
